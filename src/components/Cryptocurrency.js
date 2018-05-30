3c4470ecb449c3617264a4f80fe79f7773d495c6
Allan James Lapid <ajLapid718@gmail.com>
2018-05-30 13:49:55 -0400

    Use object destructuring to grab pertinent information and assign values semantically

--- src/components/Cryptocurrency.js
+++ src/components/Cryptocurrency.js
@@ -3,7 +3,7 @@ import './Cryptocurrency.css';
 
 class Cryptocurrency extends Component {
   render() {
-    
+    const { id, name, symbol, price_usd, percent_change_1h, percent_change_24h, percent_change_7d } = this.props.data;
   }
 }
 
