28433dc1a785c35a10e81fc1fe846d4943704038
Allan James Lapid <ajLapid718@gmail.com>
2018-05-30 13:34:11 -0400

    Fill out local state with hardcoded data

--- src/components/Tickers.js
+++ src/components/Tickers.js
@@ -2,6 +2,25 @@ import React, { Component } from 'react';
 import '.Tickers.css';
 
 class Tickers extends Component {
+  constructor(props) {
+    this.state = {
+      data: [
+        {
+          id: "bitcoin",
+          price: "1"
+        },
+        {
+          id: "ethereum",
+          price: "1"
+        },
+        {
+          id: "litecoin",
+          price: "1"
+        }
+      ]
+    }
+  }
+
 
 }
 
